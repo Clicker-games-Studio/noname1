3fdf8ee2d2d7584dbaf015eeff0cff7e29ab85b4
Clicker-games-Studio <clickergamesstudio@gmail.com>
2025-11-18 12:15:08 +0100

    update

--- game.bat
+++ game.bat
@@ -1,7 +1,9 @@
 @echo off
 title Snake Game Test
 
-REM Compile
+REM ===============================
+REM Step 1: Compile SnakeGame
+REM ===============================
 echo Compiling SnakeGame...
 java\bin\javac game1\SnakeGame.java
 if errorlevel 1 (
@@ -10,7 +12,9 @@ if errorlevel 1 (
     exit /b
 )
 
-REM Run
+REM ===============================
+REM Step 2: Run SnakeGame
+REM ===============================
 echo Running SnakeGame...
 cd game1
 ..\\java\bin\java SnakeGame
